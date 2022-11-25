ad7d67075505d33f58c1cc46a51b56cc445e58b3
tuhinahmed-cse <tuhinahmed.cse@gmail.com>
2022-11-25 22:47:59 +0600

    seller role api implement

--- index.js
+++ index.js
@@ -172,6 +172,13 @@ async function run() {
                 res.send({ isBuyer: user?.role === 'Buyer' });
             })
 
+            app.get('/users/seller/:email', async (req, res) => {
+                const email = req.params.email;
+                const query = { email }
+                const user = await usersCollection.findOne(query);
+                res.send({ isSeller: user?.role === 'Seller' });
+            })
+
 
 
 
