b07b1996e9f66f3d11fed42fe989df982c8b1580
tuhinahmed-cse <tuhinahmed.cse@gmail.com>
2022-11-25 15:22:27 +0600

    jwt sucessfully add

--- index.js
+++ index.js
@@ -16,6 +16,27 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iuz8uzh.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next) {
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
+
 async function run() {
     try {
 
@@ -57,8 +78,12 @@ async function run() {
 
 
         // Booking
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings',verifyJWT, async (req, res) => {
             const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
             const query = { email: email };
             const bookings = await bookingCollection.find(query).toArray();
             res.send(bookings);
@@ -82,7 +107,7 @@ async function run() {
             }
             res.status(403).send({ accessToken: '' })
         });
-        
+
 
         app.post('/users', async (req, res) => {
             const user = req.body;
